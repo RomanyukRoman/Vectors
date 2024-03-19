74cfe68baa2a1f1737280cc4e102e90dfa8c8787
RomanyukRoman <romaniuk.roman1121@vu.cdu.edu.ua>
2024-03-19 19:27:12 +0200

    vectors_part2

--- vectors/Assets/Move.cs
+++ vectors/Assets/Move.cs
@@ -3,13 +3,19 @@
 public class Move : MonoBehaviour {
 
     public GameObject goal;
+    Vector3 direction;
+    float speed = 2f;
 
     void Start() {
 
-        this.transform.Translate(6, 0, 2);
+        direction = goal.transform.position - this.transform.position;
+        
+        //this.transform.Translate(direction);
     }
 
-    private void Update() {
-
+    private void LateUpdate() 
+    {
+        Vector3 velocity = direction.normalized * speed * Time.deltaTime;
+        this.transform.position = this.transform.position + velocity;
     }
 }
